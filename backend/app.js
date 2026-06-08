2fe7fe695fdc51e35efa0d0e2e4b4bad8bbd5cd5
trigunprince006 <princetrigun0@gmail.com>
2026-06-08 23:17:52 +0530

    Create a working website

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const urlRoute = require('./routes/takingUrl.route')
-
+const cors = require('cors')
+app.use(cors())
 app.use(express.json())
 app.get('/',(req,res)=>{
   res.send("Server is running .....")
